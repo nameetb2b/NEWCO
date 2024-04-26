d843deacc4507f09d81e2e32dd25ad51be95e072
nameetb2b <jamblan@b2b.regn.net>
2024-04-26 13:33:35 +0530

    neco nameet

---
+++ NewCo/Script/Newco_Script.js
@@ -0,0 +1,250 @@
+﻿function Newco_WebTesting()
+{
+  //Navigates to the ''https://www.brightmine.com/uk/'' address.
+  Aliases.browser.ToUrl("https://www.brightmine.com/uk/");
+  //Waits until the browser loads the page and is ready to accept user input.
+  Aliases.browser.pageHrLeadersBrighterBusinessOut.Wait();
+  //Evaluates the visual fidelity of user interface.
+  var testRun, testRun1, testRun2, testRun3, testRun4, testRun5, testRun6, testRun7, testRun8, testRun9, testRun10, testRun11, testRun12, testRun13, testRun14, testRun15, testRun16, testRun17, testRun18, testRun19, testRun20, testRun21, testRun22, testRun23, testRun24;
+  testRun = VisualTest.CreateRun("Newco_WebTesting");
+  testRun.SetAllowedFails(0);
+  testRun.Capture(Aliases.browser.pageHrLeadersBrighterBusinessOut, "HomePage", testRun.Fullpage, 0, true);
+  //Clicks the 'textnodeSolutions3' control.
+  Aliases.browser.pageHrLeadersBrighterBusinessOut.headerSolutions.textnodeSolutions.textnodeSolutions2.textnodeSolutions3.Click();
+  //Clicks the 'linkLearnAboutAllOurSolutions' link.
+  Aliases.browser.pageHrLeadersBrighterBusinessOut.headerSolutions.textnodeSolutions.textnodeLearnAboutAllOurSolution.linkLearnAboutAllOurSolutions.Click();
+  //Waits until the browser loads the page and is ready to accept user input.
+  Aliases.browser.pageHrSolutionsBuiltOnDataAnalyt.Wait();
+  //Evaluates the visual fidelity of user interface.
+  testRun1 = VisualTest.CreateRun("Newco_WebTesting");
+  testRun1.SetAllowedFails(0);
+  testRun1.Capture(Aliases.browser.pageHrSolutionsBuiltOnDataAnalyt, "AllSolutions", testRun1.Fullpage, 0, true);
+  //Clicks the 'textnodeSolutions3' control.
+  Aliases.browser.pageHrSolutionsBuiltOnDataAnalyt.headerSolutions.textnodeSolutions.textnodeSolutions2.textnodeSolutions3.Click();
+  //Clicks the 'linkAttractRetainTopTalent' link.
+  Aliases.browser.pageHrSolutionsBuiltOnDataAnalyt.headerSolutions.textnodeSolutions.textnodeSolutions2.linkAttractRetainTopTalent.Click();
+  //Waits until the browser loads the page and is ready to accept user input.
+  Aliases.browser.pageHrSolutionsBuiltOnDataAnalyt2.Wait();
+  //Evaluates the visual fidelity of user interface.
+  testRun2 = VisualTest.CreateRun("Newco_WebTesting");
+  testRun2.SetAllowedFails(0);
+  testRun2.Capture(Aliases.browser.pageHrSolutionsBuiltOnDataAnalyt2, "LinkAttractRetain", testRun2.Fullpage, 0, true);
+  //Clicks the 'textnodeSolutions3' control.
+  Aliases.browser.pageHrSolutionsBuiltOnDataAnalyt2.headerSolutions.textnodeSolutions.textnodeSolutions2.textnodeSolutions3.Click();
+  //Clicks the 'linkIncreaseEfficiency' link.
+  Aliases.browser.pageHrSolutionsBuiltOnDataAnalyt2.headerSolutions.textnodeSolutions.textnodeSolutions2.linkIncreaseEfficiency.Click();
+  //Waits until the browser loads the page and is ready to accept user input.
+  Aliases.browser.pageWwwBrightmineComUkSolutionsE.Wait();
+  //Evaluates the visual fidelity of user interface.
+  testRun3 = VisualTest.CreateRun("Newco_WebTesting");
+  testRun3.SetAllowedFails(0);
+  testRun3.Capture(Aliases.browser.pageWwwBrightmineComUkSolutionsE, "IncreaseEfficiency", testRun3.Fullpage, 0, true);
+  //Clicks the 'textnodeSolutions3' control.
+  Aliases.browser.pageWwwBrightmineComUkSolutionsE.headerSolutions.textnodeSolutions.textnodeSolutions2.textnodeSolutions3.Click();
+  //Clicks the 'linkDeliverGreaterBusinessImpact' link.
+  Aliases.browser.pageWwwBrightmineComUkSolutionsE.headerSolutions.textnodeSolutions.textnodeSolutions2.linkDeliverGreaterBusinessImpact.Click();
+  //Waits until the browser loads the page and is ready to accept user input.
+  Aliases.browser.pageWwwBrightmineComUkSolutionsB.Wait();
+  //Evaluates the visual fidelity of user interface.
+  testRun4 = VisualTest.CreateRun("Newco_WebTesting");
+  testRun4.SetAllowedFails(0);
+  testRun4.Capture(Aliases.browser.pageWwwBrightmineComUkSolutionsB, "DeliverGreatBusiness", testRun4.Fullpage, 0, true);
+  //Clicks the 'textnodeSolutions3' control.
+  Aliases.browser.pageWwwBrightmineComUkSolutionsB.headerSolutions.textnodeSolutions.textnodeSolutions2.textnodeSolutions3.Click();
+  //Clicks the 'linkReduceComplianceRisk' link.
+  Aliases.browser.pageWwwBrightmineComUkSolutionsB.headerSolutions.textnodeSolutions.textnodeSolutions2.linkReduceComplianceRisk.Click();
+  //Waits until the browser loads the page and is ready to accept user input.
+  Aliases.browser.pageDeliverHrThatMakesImpactAcro.Wait();
+  //Evaluates the visual fidelity of user interface.
+  testRun5 = VisualTest.CreateRun("Newco_WebTesting");
+  testRun5.SetAllowedFails(0);
+  testRun5.Capture(Aliases.browser.pageDeliverHrThatMakesImpactAcro, "ReduceComplianceRisk", testRun5.Fullpage, 0, true);
+  //Clicks the 'textnodeSolutions3' control.
+  Aliases.browser.pageDeliverHrThatMakesImpactAcro.headerSolutions.textnodeSolutions.textnodeSolutions2.textnodeSolutions3.Click();
+  //Clicks the 'linkCreateEquitableReward' link.
+  Aliases.browser.pageDeliverHrThatMakesImpactAcro.headerSolutions.textnodeSolutions.textnodeSolutions2.linkCreateEquitableReward.Click();
+  //Waits until the browser loads the page and is ready to accept user input.
+  Aliases.browser.pageReduceRiskStayCompliantWithI.Wait();
+  //Evaluates the visual fidelity of user interface.
+  testRun6 = VisualTest.CreateRun("Newco_WebTesting");
+  testRun6.SetAllowedFails(0);
+  testRun6.Capture(Aliases.browser.pageReduceRiskStayCompliantWithI, "EquitableReward", testRun6.Fullpage, 0, true);
+  //Clicks the 'textnodeSolutions3' control.
+  Aliases.browser.pageReduceRiskStayCompliantWithI.headerSolutions.textnodeSolutions.textnodeSolutions2.textnodeSolutions3.Click();
+  //Clicks the 'linkLargeEnterprise' link.
+  Aliases.browser.pageReduceRiskStayCompliantWithI.headerSolutions.textnodeSolutions.textnodeSolutions2.linkLargeEnterprise.Click();
+  //Waits until the browser loads the page and is ready to accept user input.
+  Aliases.browser.pageWwwBrightmineComUkSolutionsE2.Wait();
+  //Evaluates the visual fidelity of user interface.
+  testRun7 = VisualTest.CreateRun("Newco_WebTesting");
+  testRun7.SetAllowedFails(0);
+  testRun7.Capture(Aliases.browser.pageWwwBrightmineComUkSolutionsE2, "LargeEnterprise", testRun7.Fullpage, 0, true);
+  //Clicks the 'textnodeSolutions3' control.
+  Aliases.browser.pageWwwBrightmineComUkSolutionsE2.headerSolutions.textnodeSolutions.textnodeSolutions2.textnodeSolutions3.Click();
+  //Clicks the 'linkSmallMidSize' link.
+  Aliases.browser.pageWwwBrightmineComUkSolutionsE2.headerSolutions.textnodeSolutions.textnodeSolutions2.linkSmallMidSize.Click();
+  //Waits until the browser loads the page and is ready to accept user input.
+  Aliases.browser.pageWwwBrightmineComUkSolutionsM.Wait();
+  //Evaluates the visual fidelity of user interface.
+  testRun8 = VisualTest.CreateRun("Newco_WebTesting");
+  testRun8.SetAllowedFails(0);
+  testRun8.Capture(Aliases.browser.pageWwwBrightmineComUkSolutionsM, "SmallMidSizeBusiness", testRun8.Fullpage, 0, true);
+  //Clicks the 'textnodeSolutions3' control.
+  Aliases.browser.pageWwwBrightmineComUkSolutionsM.headerSolutions.textnodeSolutions.textnodeSolutions2.textnodeSolutions3.Click();
+  //Clicks the 'linkChro' link.
+  Aliases.browser.pageWwwBrightmineComUkSolutionsM.headerSolutions.textnodeSolutions.textnodeSolutions2.linkChro.Click();
+  //Waits until the browser loads the page and is ready to accept user input.
+  Aliases.browser.pageAgileHrSolutionsForBusinesse.Wait();
+  //Evaluates the visual fidelity of user interface.
+  testRun9 = VisualTest.CreateRun("Newco_WebTesting");
+  testRun9.SetAllowedFails(0);
+  testRun9.Capture(Aliases.browser.pageAgileHrSolutionsForBusinesse, "chro", testRun9.Fullpage, 0, true);
+  //Clicks the 'textnodeSolutions3' control.
+  Aliases.browser.pageAgileHrSolutionsForBusinesse.headerSolutions.textnodeSolutions.textnodeSolutions2.textnodeSolutions3.Click();
+  //Clicks the 'linkHrOperationsAndCompliance' link.
+  Aliases.browser.pageAgileHrSolutionsForBusinesse.headerSolutions.textnodeSolutions.textnodeSolutions2.linkHrOperationsAndCompliance.Click();
+  //Waits until the browser loads the page and is ready to accept user input.
+  Aliases.browser.pageHelpingChrosAchieveBrighterB.Wait();
+  //Evaluates the visual fidelity of user interface.
+  testRun10 = VisualTest.CreateRun("Newco_WebTesting");
+  testRun10.SetAllowedFails(0);
+  testRun10.Capture(Aliases.browser.pageHelpingChrosAchieveBrighterB, "HROperation", testRun10.Fullpage, 0, true);
+  //Clicks the 'textnodeSolutions3' control.
+  Aliases.browser.pageHelpingChrosAchieveBrighterB.headerSolutions.textnodeSolutions.textnodeSolutions2.textnodeSolutions3.Click();
+  //Clicks the 'linkTotalRewardsAndCompensation' link.
+  Aliases.browser.pageHelpingChrosAchieveBrighterB.headerSolutions.textnodeSolutions.textnodeSolutions2.linkTotalRewardsAndCompensation.Click();
+  //Waits until the browser loads the page and is ready to accept user input.
+  Aliases.browser.pageWwwBrightmineComUkSolutionsT.Wait();
+  //Evaluates the visual fidelity of user interface.
+  testRun11 = VisualTest.CreateRun("Newco_WebTesting");
+  testRun11.SetAllowedFails(0);
+  testRun11.Capture(Aliases.browser.pageWwwBrightmineComUkSolutionsT, "TotalRewards", testRun11.Fullpage, 0, true);
+  //Clicks the 'textnodeSolutions3' control.
+  Aliases.browser.pageWwwBrightmineComUkSolutionsT.headerSolutions.textnodeSolutions.textnodeSolutions2.textnodeSolutions3.Click();
+  //Clicks the 'linkPeopleAnalytics' link.
+  Aliases.browser.pageWwwBrightmineComUkSolutionsT.headerSolutions.textnodeSolutions.textnodeSolutions2.linkPeopleAnalytics.Click();
+  //Waits until the browser loads the page and is ready to accept user input.
+  Aliases.browser.pageWwwBrightmineComUkSolutionsP.Wait();
+  //Evaluates the visual fidelity of user interface.
+  testRun12 = VisualTest.CreateRun("Newco_WebTesting");
+  testRun12.SetAllowedFails(0);
+  testRun12.Capture(Aliases.browser.pageWwwBrightmineComUkSolutionsP, "PeopleAnalytics", testRun12.Fullpage, 0, true);
+  //Clicks the 'textnodeSolutions3' control.
+  Aliases.browser.pageWwwBrightmineComUkSolutionsP.headerSolutions.textnodeSolutions.textnodeSolutions2.textnodeSolutions3.Click();
+  //Clicks the 'linkHrConsultantHroPeo' link.
+  Aliases.browser.pageWwwBrightmineComUkSolutionsP.headerSolutions.textnodeSolutions.textnodeSolutions2.linkHrConsultantHroPeo.Click();
+  //Waits until the browser loads the page and is ready to accept user input.
+  Aliases.browser.pageWwwBrightmineComUkSolutionsH.Wait();
+  //Evaluates the visual fidelity of user interface.
+  testRun13 = VisualTest.CreateRun("Newco_WebTesting");
+  testRun13.SetAllowedFails(0);
+  testRun13.Capture(Aliases.browser.pageWwwBrightmineComUkSolutionsH, "HrConsultant", testRun13.Fullpage, 0, true);
+  //Clicks the 'textnodeSolutions3' control.
+  Aliases.browser.pageWwwBrightmineComUkSolutionsH.headerSolutions.textnodeSolutions.textnodeSolutions2.textnodeSolutions3.Click();
+  //Clicks the 'textnodeProducts2' control.
+  Aliases.browser.pageWwwBrightmineComUkSolutionsH.headerSolutions.textnodeSolutions.textnodeProducts.textnodeProducts2.Click();
+  //Clicks the 'linkHrComplianceCentre' link.
+  Aliases.browser.pageWwwBrightmineComUkSolutionsH.headerSolutions.textnodeSolutions.textnodeProducts.linkHrComplianceCentre.Click();
+  //Waits until the browser loads the page and is ready to accept user input.
+  Aliases.browser.pageWwwBrightmineComUkProductsHr.Wait();
+  //Evaluates the visual fidelity of user interface.
+  testRun14 = VisualTest.CreateRun("Newco_WebTesting");
+  testRun14.SetAllowedFails(0);
+  testRun14.Capture(Aliases.browser.pageWwwBrightmineComUkProductsHr, "HrCompliaceCentre", testRun14.Fullpage, 0, true);
+  //Clicks the 'textnodeSolutions3' control.
+  Aliases.browser.pageWwwBrightmineComUkSolutionsH.headerSolutions.textnodeSolutions.textnodeSolutions2.textnodeSolutions3.Click();
+  //Clicks the 'linkAutomatedHandbookManagement' link.
+  Aliases.browser.pageWwwBrightmineComUkProductsHr.headerSolutions.textnodeSolutions.textnodeProducts.linkAutomatedHandbookManagement.Click();
+  //Waits until the browser loads the page and is ready to accept user input.
+  Aliases.browser.pageWwwBrightmineComUkProductsAu.Wait();
+  //Evaluates the visual fidelity of user interface.
+  testRun15 = VisualTest.CreateRun("Newco_WebTesting");
+  testRun15.SetAllowedFails(0);
+  testRun15.Capture(Aliases.browser.pageWwwBrightmineComUkProductsAu, "AutomatedHandbook", testRun15.Fullpage, 0, true);
+  //Clicks the 'textnodeProducts2' control.
+  Aliases.browser.pageWwwBrightmineComUkProductsAu.headerSolutions.textnodeSolutions.textnodeProducts.textnodeProducts2.Click();
+  //Clicks the 'linkCollectiveLabourAgreements' link.
+  Aliases.browser.pageWwwBrightmineComUkProductsAu.headerSolutions.textnodeSolutions.textnodeProducts.linkCollectiveLabourAgreements.Click();
+  //Waits until the browser loads the page and is ready to accept user input.
+  Aliases.browser.pageWwwBrightmineComUkProductsCo.Wait();
+  //Evaluates the visual fidelity of user interface.
+  testRun16 = VisualTest.CreateRun("Newco_WebTesting");
+  testRun16.SetAllowedFails(0);
+  testRun16.Capture(Aliases.browser.pageWwwBrightmineComUkProductsCo, "CollectiveLabourAgreement", testRun16.Fullpage, 0, true);
+  //Clicks the 'textnodeProducts2' control.
+  Aliases.browser.pageWwwBrightmineComUkProductsCo.headerSolutions.textnodeSolutions.textnodeProducts.textnodeProducts2.Click();
+  //Clicks the 'linkCompensationPlanning' link.
+  Aliases.browser.pageWwwBrightmineComUkProductsCo.headerSolutions.textnodeSolutions.textnodeProducts.linkCompensationPlanning.Click();
+  //Waits until the browser loads the page and is ready to accept user input.
+  Aliases.browser.pageAccessVitalRealTimeDataWithC.Wait();
+  //Evaluates the visual fidelity of user interface.
+  testRun17 = VisualTest.CreateRun("Newco_WebTesting");
+  testRun17.SetAllowedFails(0);
+  testRun17.Capture(Aliases.browser.pageAccessVitalRealTimeDataWithC, "CompensationPlanning", testRun17.Fullpage, 0, true);
+  //Clicks the 'textnodeProducts2' control.
+  Aliases.browser.pageAccessVitalRealTimeDataWithC.headerSolutions.textnodeSolutions.textnodeProducts.textnodeProducts2.Click();
+  //Clicks the 'linkPayEquityAnalytics' link.
+  Aliases.browser.pageAccessVitalRealTimeDataWithC.headerSolutions.textnodeSolutions.textnodeProducts.linkPayEquityAnalytics.Click();
+  //Waits until the browser loads the page and is ready to accept user input.
+  Aliases.browser.pageAttractAndRetainTopTalentWit.Wait();
+  //Evaluates the visual fidelity of user interface.
+  testRun18 = VisualTest.CreateRun("Newco_WebTesting");
+  testRun18.SetAllowedFails(0);
+  testRun18.Capture(Aliases.browser.pageAttractAndRetainTopTalentWit, "PayEquityAnalytics", testRun18.Fullpage, 0, true);
+  //Clicks the 'textnodeProducts2' control.
+  Aliases.browser.pageAttractAndRetainTopTalentWit.headerSolutions.textnodeSolutions.textnodeProducts.textnodeProducts2.Click();
+  //Clicks the 'textnodeAboutUs2' control.
+  Aliases.browser.pageAttractAndRetainTopTalentWit.headerSolutions.textnodeSolutions.textnodeAboutUs.textnodeAboutUs2.Click();
+  //Clicks the 'linkAboutUs' link.
+  Aliases.browser.pageAttractAndRetainTopTalentWit.headerSolutions.textnodeSolutions.textnodeAboutUs.linkAboutUs.Click();
+  //Waits until the browser loads the page and is ready to accept user input.
+  Aliases.browser.pageTrackManageAndClosePayGapsWi.Wait();
+  //Evaluates the visual fidelity of user interface.
+  testRun19 = VisualTest.CreateRun("Newco_WebTesting");
+  testRun19.SetAllowedFails(0);
+  testRun19.Capture(Aliases.browser.pageTrackManageAndClosePayGapsWi, "AboutUs", testRun19.Fullpage, 0, true);
+  //Clicks the 'textnodeAboutUs2' control.
+  Aliases.browser.pageTrackManageAndClosePayGapsWi.headerSolutions.textnodeSolutions.textnodeAboutUs.textnodeAboutUs2.Click();
+  //Clicks the 'linkWhyChooseBrightmine' link.
+  Aliases.browser.pageTrackManageAndClosePayGapsWi.headerSolutions.textnodeSolutions.textnodeAboutUs.linkWhyChooseBrightmine.Click();
+  //Waits until the browser loads the page and is ready to accept user input.
+  Aliases.browser.pageEmpoweringHrLeadersToDeliver.Wait();
+  //Evaluates the visual fidelity of user interface.
+  testRun20 = VisualTest.CreateRun("Newco_WebTesting");
+  testRun20.SetAllowedFails(0);
+  testRun20.Capture(Aliases.browser.pageEmpoweringHrLeadersToDeliver, "Whybrightmine", testRun20.Fullpage, 0, true);
+  //Clicks the 'linkResources' link.
+  Aliases.browser.pageEmpoweringHrLeadersToDeliver.headerSolutions.textnodeSolutions.textnodeResources.linkResources.Click();
+  //Waits until the browser loads the page and is ready to accept user input.
+  Aliases.browser.pageChooseAProvenHrPartnerWithOv.Wait();
+  //Evaluates the visual fidelity of user interface.
+  testRun21 = VisualTest.CreateRun("Newco_WebTesting");
+  testRun21.SetAllowedFails(0);
+  testRun21.Capture(Aliases.browser.pageChooseAProvenHrPartnerWithOv, "Resoucres", testRun21.Fullpage, 0, true);
+  //Clicks the 'textnodeCommunity2' control.
+  Aliases.browser.pageChooseAProvenHrPartnerWithOv.headerSolutions.textnodeSolutions.textnodeCommunity.textnodeCommunity2.Click();
+  //Clicks the 'linkCaseStudies' link.
+  Aliases.browser.pageChooseAProvenHrPartnerWithOv.headerSolutions.textnodeSolutions.textnodeCommunity.linkCaseStudies.Click();
+  //Waits until the browser loads the page and is ready to accept user input.
+  Aliases.browser.pageWwwBrightmineComUkCaseStudie.Wait();
+  //Evaluates the visual fidelity of user interface.
+  testRun22 = VisualTest.CreateRun("Newco_WebTesting");
+  testRun22.SetAllowedFails(0);
+  testRun22.Capture(Aliases.browser.pageWwwBrightmineComUkCaseStudie, "CaseStudies", testRun22.Fullpage, 0, true);
+  //Clicks the 'textnodeCommunity2' control.
+  Aliases.browser.pageWwwBrightmineComUkCaseStudie.headerSolutions.textnodeSolutions.textnodeCommunity.textnodeCommunity2.Click();
+  //Clicks the 'linkPartners' link.
+  Aliases.browser.pageWwwBrightmineComUkCaseStudie.headerSolutions.textnodeSolutions.textnodeCommunity.linkPartners.Click();
+  //Waits until the browser loads the page and is ready to accept user input.
+  Aliases.browser.pageWwwBrightmineComUkPartners.Wait();
+  //Evaluates the visual fidelity of user interface.
+  testRun23 = VisualTest.CreateRun("Newco_WebTesting");
+  testRun23.SetAllowedFails(0);
+  testRun23.Capture(Aliases.browser.pageWwwBrightmineComUkPartners, "Partners", testRun23.Fullpage, 0, true);
+  //Clicks the 'linkBookDemo' link.
+  Aliases.browser.pageWwwBrightmineComUkPartners.headerSolutions.textnodeSolutions.textnodeBookDemo.linkBookDemo.Click();
+  //Evaluates the visual fidelity of user interface.
+  testRun24 = VisualTest.CreateRun("Newco_WebTesting");
+  testRun24.SetAllowedFails(0);
+  testRun24.Capture(Aliases.browser.pageRequestADemoBrightmineUk, "BookDemo", testRun24.Fullpage, 0, true);
+}
---
+++ NewCo/Script/Newco_Solutions.js
@@ -0,0 +1,14 @@
+﻿function Newco_Solutions()
+{
+  //Clicks the 'BrowserWindow' object.
+  Aliases.browser.BrowserWindow.Click(1302, 30);
+  //Evaluates the visual fidelity of user interface.
+  var testRun;
+  testRun = VisualTest.CreateRun("Newco_Solutions");
+  testRun.SetAllowedFails(0);
+  testRun.Capture(Aliases.browser.pageHrSolutionsBuiltOnDataAnalyt, "solutiondemo", testRun.Fullpage, 0, true);
+  //Opens the specified URL in a running instance of the specified browser.
+  Browsers.Item(btChrome).Navigate("https://www.brightmine.com/uk/solutions/");
+  //Maximizes the specified Window object.
+  Aliases.browser.BrowserWindow.Maximize();
+}
